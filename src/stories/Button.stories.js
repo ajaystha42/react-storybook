b19e31310bb88b43a14b38dfdaa33d0ae1fc49e5
Ajay Shrestha <er.ajaystha@gmail.com>
2023-05-10 10:32:43 -0400

    storybook actions implementation

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,39 +1,15 @@
-import { Button } from './Button';
+import Button from "./../components/Button";
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
-  title: 'Example/Button',
+  title: "Button",
   component: Button,
-  tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
-};
+const Template = (args) => <Button {...args} />;
 
-export const Secondary = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
+export const Red = Template.bind({});
+Red.args = {
+  backgroundColor: "red",
+  label: "Press Me",
+  size: "md",
 };
